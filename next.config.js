f5484778090519bc86e8c93b29692f38c4fe9536
Joel <contact_jhern@proton.me>
2026-07-23 15:03:19 -0700

    chore: remove commented-out config from tailwind and next configs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
   };
